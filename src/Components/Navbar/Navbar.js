5308e760dbf32367a378d229429af6d5c89f40dd
Aarushi Shanker <aarushishanker3108@gmail.com>
2021-10-20 14:11:58 +0530

    Cart product popover done

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Navbar.css'
-import {Navbar,NavDropdown,Nav,Container} from 'react-bootstrap'
+import {Navbar,NavDropdown,Nav,Container,OverlayTrigger} from 'react-bootstrap'
 
 
 function navbar() {
+
+    const popover=()=>{
+      return(
+        <div className="popover">
+          <div className="cart-product">
+            <img src="https://d1wwyfhxuarwk4.cloudfront.net/images/products/common/white/xl/4436-32-w_fun-shades-sunglasses-navy-blue4e76410a050c142b3b7a399cc8b96340.jpg" alt="glasses"/>
+            <span>White Oval Full Rim TR-90 Frame-Computer Spex (Zero Power)</span>
+          </div>
+          <p className="price">1 x  ₹799</p>
+          <p className="subtotal">Subtotal : ₹799</p>
+          <p className="links">View Cart</p>
+          <p className="links">Checkout</p>
+        </div>
+      )
+    }
+
+
     return (
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
   <Container>
@@ -29,7 +46,24 @@ function navbar() {
       <Nav.Link href="#memes">
       <i class="fab fa-facebook"></i>
       </Nav.Link>
-      <Nav.Link><i class="fas fa-shopping-bag" style={{fontSize:'30px'}}></i></Nav.Link>
+      <>
+  
+    <OverlayTrigger
+      
+      key="bottom"
+      placement="bottom"
+      overlay={
+        // <Tooltip id="tooltip-bottom">
+        //   cart
+        // </Tooltip>
+        popover()
+      }
+    >
+            <Nav.Link><i class="fas fa-shopping-bag" style={{fontSize:'30px'}}></i></Nav.Link>
+    </OverlayTrigger>
+
+</>
+
       <Nav.Link><i class="fas fa-user-circle" style={{fontSize:'30px'}}></i></Nav.Link>
     </Nav>
   </Navbar.Collapse>
