17a718e388229644a3ccd0166688b3ff101040a1
Aarushi Shanker <aarushishanker3108@gmail.com>
2021-10-29 11:17:39 +0530

    Cart modified

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -15,35 +15,44 @@ function Cart() {
                         <Col className="col-heading">Subtotal</Col>
                     </Row>
                     <Row style={{borderBottom:'2px solid black',padding:'20px 0'}}>
-                        <Col xs={6} className="col-content"><img src="https://d1wwyfhxuarwk4.cloudfront.net/images/products/common/white/xl/4436-32-w_fun-shades-sunglasses-navy-blue4e76410a050c142b3b7a399cc8b96340.jpg" alt="product" style={{width:'75px'}}/>White Oval Full Rim TR-90 Frame-Computer Spex (Zero Power)</Col>
+                        <Col xs={6} className="col-content"><img src="https://d1wwyfhxuarwk4.cloudfront.net/images/products/common/white/xl/4436-32-w_fun-shades-sunglasses-navy-blue4e76410a050c142b3b7a399cc8b96340.jpg" alt="product" style={{width:'75px'}}/><div className="item"><span>White Oval Full Rim TR-90 Frame-Computer Spex (Zero Power)</span><span className="remove">Remove</span></div></Col>
                         <Col className="col-content" style={{marginTop:'23px'}}>₹799</Col>
                         <Col className="col-content" style={{marginTop:'23px'}}>1</Col>
                         <Col className="col-content" style={{marginTop:'23px'}}>₹799</Col>
                     </Row>
                     <Row style={{padding:'20px 0'}}>
-                        <Col><input placeholder="Coupon Code" type="text" style={{padding:'10px'}}/></Col>
-                        <Col xs={6} className="apply-coupon">Apply coupon</Col>
-                        <Col className="col-content">Update Cart</Col>
+                        <Col xs={6} className="apply-coupon"><textarea placeholder="Any special instructions..." style={{width:'80%',height:'100px',padding:'10px'}}/></Col>
+                        <Col className="col-content" style={{borderLeft:'2px solid black'}}><div style={{display:'flex',flexDirection:'column',paddingLeft:'100px'}}>
+                                <input className="apply-coupon" placeholder="Coupon Code"/>
+                                <button className="apply-coupon">Apply</button>
+                            </div></Col>
                     </Row>
-                </Container>
+                </Container> 
             </div>
             <div className="cart-total">
-                <Container style={{width:'50vw',marginLeft:'44%'}}>
-                    <Row style={{border:'0.2px solid rgb(230, 230, 230)',padding:'20px 0',background:'#FBFBFB'}}>
-                        <Col xs={12} className="cart-heading" style={{fontSize:'24px'}}>Cart Total</Col>
-                    </Row>
-                    <Row style={{border:'0.2px solid rgb(230, 230, 230)',padding:'20px 0'}}>
-                        <Col className="col-heading">Subtotal</Col>
-                        <Col className="col-heading">₹799</Col>
-                    </Row>
-                    <Row style={{border:'0.2px solid rgb(230, 230, 230)',padding:'20px 0',background:'#FBFBFB'}}>
-                        <Col className="col-heading">Total</Col>
-                        <Col className="col-heading">₹799</Col>
-                    </Row>
-                    <Row style={{border:'0.2px solid rgb(230, 230, 230)',padding:'20px 0'}}>
-                        <Col className="apply-coupon" style={{textAlign:'center'}}>Proceed to checkout</Col>
-                    </Row>
-                </Container>
+                <h3 className="cart-heading">Cart total</h3>
+                <div className="cart-total-container">
+                    <div className="receipt">
+                        <Container>
+                            <Row>
+                                <Col>Item Total</Col>
+                                <Col>₹ 1598</Col>
+                            </Row>
+                            <Row style={{marginTop:'30px'}}>
+                                <Col>Delivery</Col>
+                                <Col>₹ 0</Col>
+                            </Row>
+                            <Row style={{marginTop:'30px'}}>
+                                <Col>Total</Col>
+                                <Col>₹ 1598</Col>
+                            </Row>
+                        </Container>
+                    </div>
+                    <div className="cart-buttons">
+                        <button className="checkout">Proceed to checkout</button>
+                        <button className="apply-coupon">Continue shopping</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
